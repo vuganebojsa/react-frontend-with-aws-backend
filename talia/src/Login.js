72f78bb85bc5cbb7b684b6021469b4e529061fef
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-07-25 19:36:58 +0200

    Added login design

--- talia/src/Login.js
+++ talia/src/Login.js
@@ -1,6 +1,38 @@
 
 function Login(){
-    return (<><h1 className="main-text-color">TALIJA</h1></>);
+    return (
+    <div className="login talia-border">
+        <div className="row text-center mx-2">
+            <div className="col-12">
+            <h3 className="main-text-color p-3">Ulogujte se</h3>
+
+            </div>
+        </div>
+        <form>
+            <div className="row text-center">
+                <div className="col-12">
+                    <div class="form-group form-field">
+                        <label for="email">Email adresa</label>
+                        <input type="email" className="form-control text-center talia-input" name="email"  placeholder="mara@primer.com"/>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div class="form-group form-field">
+                        <label for="password">Lozinka</label>
+                        <input type="password" className="form-control text-center talia-input" placeholder="******" name="password"/>
+            
+                    </div>
+                </div>
+
+                <div className="col-12">
+                    <button type="submit" class="talia-button my-3 talia-border">Ulogujte se</button>
+
+
+                </div>
+            </div>
+        </form>
+    </div>
+    );
 }
 
 export default Login;
